488cae0a61355cc8136aeb8858694015b89ad497
Michael <drmfloren@gmail.com>
2024-07-24 13:52:59 -0500

    Fixing a typo

--- R/fixp.R
+++ R/fixp.R
@@ -8,7 +8,7 @@
 #' @param dig The number of digits you wish to round to
 #' 
 #' 
-#' @details Thus function expects summary tables (and turns them into a data.frame). It assumes that the last column of the table contains p-values (with the name Pr..., normal in many summary tables). It will shoot a warning letting you know if you are using the function with a table that doesn't have a name that starts with this for the last column, though it will still run. The function takes the data frame and manipulates all values that round to 0 (at the set digits) to instead display that the p-value is less than the current rounded digits outcome. Note that this makes the last column of the output dataframe either character or numeric.
+#' @details This function expects summary tables (and turns them into a data.frame). It assumes that the last column of the table contains p-values (with the name Pr..., normal in many summary tables). It will shoot a warning letting you know if you are using the function with a table that doesn't have a name that starts with this for the last column, though it will still run. The function takes the data frame and manipulates all values that round to 0 (at the set digits) to instead display that the p-value is less than the current rounded digits outcome. Note that this makes the last column of the output dataframe either character or numeric.
 #' 
 #' @examples 
 #' #this gives a summary table with a small p-value
